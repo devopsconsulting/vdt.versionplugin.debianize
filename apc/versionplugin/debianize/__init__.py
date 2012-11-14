2dda55c4f65129db73b86b4855ae6b4f4a3faad1
Lars van de Kerkhof <lars@permanentmarkers.nl>
2012-11-14 11:23:28 +0100

    import correct functions.

--- apc/versionplugin/debianize/__init__.py
+++ apc/versionplugin/debianize/__init__.py
@@ -1,1 +1,2 @@
-from apc.versionplugin.default.package import build_package, set_package_version
+from apc.versionplugin.debianize.version import get_version
+from apc.versionplugin.debianize.package import build_package, set_package_version
