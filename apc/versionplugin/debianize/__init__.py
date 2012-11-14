6adfcdf11938023b9d03e10b6c29977e19975dd9
Lars van de Kerkhof <lars@permanentmarkers.nl>
2012-11-14 13:09:38 +0100

    The plugins have nothing to do with logging config.

--- apc/versionplugin/debianize/__init__.py
+++ apc/versionplugin/debianize/__init__.py
@@ -1,6 +1,2 @@
-import logging
-
-logging.basicConfig(level=logging.DEBUG)
-
 from apc.versionplugin.debianize.version import get_version
 from apc.versionplugin.debianize.package import build_package, set_package_version
