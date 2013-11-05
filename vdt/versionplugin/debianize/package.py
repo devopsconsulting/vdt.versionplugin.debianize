ab309e7f19dea1b560e820d8434b3e184dfa691a
Lars van de Kerkhof <lars@permanentmarkers.nl>
2013-11-05 15:07:58 +0100

    Fixes include arguments.

--- vdt/versionplugin/debianize/package.py
+++ vdt/versionplugin/debianize/package.py
@@ -11,11 +11,13 @@ def build_package(version):
     Build package with debianize.
     """
     args, extra_args = parse_version_extra_args(version.extra_args)
-    includes = []
-    for include in args.include:
-        includes.append('-i')
-        includes.append(include)
 
+    includes = []
+    if args.include:
+        for include in args.include:
+            includes.append('-i')
+            includes.append(include)
+    
     log.debug("Building version {0} with debianize.".format(version))
     with version.checkout_tag:
         subprocess.check_call(['debianize.sh'] + includes + ['--version=%s' % version, '--python-install-lib=/usr/lib/python2.7/dist-packages/'] + extra_args)
--- vdt/versionplugin/debianize/shared.py
+++ vdt/versionplugin/debianize/shared.py
@@ -4,7 +4,7 @@ from vdt.version.shared import VersionError
 
 def parse_version_extra_args(version_args):
     p = argparse.ArgumentParser(description="Package python packages with debianize.sh.")
-    p.add_argument('--include','-i', action='append', nargs='*', help="Using this flag makes following dependencies explicit. It will only build dependencies listed in install_requires that match the regex specified after -i. Use -i multiple times to specify multiple packages")
+    p.add_argument('--include','-i', action='append', help="Using this flag makes following dependencies explicit. It will only build dependencies listed in install_requires that match the regex specified after -i. Use -i multiple times to specify multiple packages")
     args, extra_args = p.parse_known_args(version_args)
     
     return args, extra_args
