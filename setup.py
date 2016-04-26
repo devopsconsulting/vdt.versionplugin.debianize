3593ece5d76b920370cf3103058bab158613b686
Lars van de Kerkhof <lars@permanentmarkers.nl>
2016-04-26 10:43:13 +0200

    new version.

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import find_packages, setup
 pkgname = "vdt.versionplugin.debianize"
 
 setup(name=pkgname,
-      version="0.0.7",
+      version="1.0.0",
       description="Version Increment Plugin that builds with debianize",
       author="Lars van de Kerkhof",
       author_email="lars@permanentmarkers.nl",
