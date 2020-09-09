edbe59a7e3af3f60680b2a5f668319cbede05634
Martijn Jacobs <maerteijn@gmail.com>
2020-09-09 11:39:05 +0200

    Depend on vdt.versionplugin.wheel for determining the correct version

--- setup.py
+++ setup.py
@@ -3,26 +3,28 @@ from setuptools import find_packages, setup
 
 pkgname = "vdt.versionplugin.debianize"
 
-setup(name=pkgname,
-      version="1.0.6",
-      description="Version Increment Plugin that builds with debianize",
-      author="Lars van de Kerkhof",
-      author_email="lars@permanentmarkers.nl",
-      maintainer="Lars van de Kerkhof",
-      maintainer_email="lars@permanentmarkers.nl",
-      packages=find_packages(),
-      include_package_data=True,
-      namespace_packages=['vdt', 'vdt.versionplugin'],
-      zip_safe=True,
-      install_requires=[
-          "setuptools",
-          "vdt.version>=0.1.4",
-          "vdt.versionplugin.default",
-          "pip==10.0.1"
-      ],
-      entry_points={
-        'console_scripts':[
-            'vdt.fpmeditor = vdt.versionplugin.debianize.fpmeditor:main',
+setup(
+    name=pkgname,
+    version="1.0.7",
+    description="Version Increment Plugin that builds with debianize",
+    author="Lars van de Kerkhof",
+    author_email="lars@permanentmarkers.nl",
+    maintainer="Lars van de Kerkhof",
+    maintainer_email="lars@permanentmarkers.nl",
+    packages=find_packages(),
+    include_package_data=True,
+    namespace_packages=["vdt", "vdt.versionplugin"],
+    zip_safe=True,
+    install_requires=[
+        "setuptools",
+        "vdt.version>=0.1.4",
+        "vdt.versionplugin.default",
+        "vdt.versionplugin.wheel>=0.0.7",
+        "pip==10.0.1",
+    ],
+    entry_points={
+        "console_scripts": [
+            "vdt.fpmeditor = vdt.versionplugin.debianize.fpmeditor:main",
         ]
-      },
+    },
 )
